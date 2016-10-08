8170d45283aab803f9503519e4bcc6561ba505b6
kengrary <kengrary@126.com>
2016-10-08 22:18:36 +0800

    version 0.8

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ PORT = 5001
 CSRF_ENABLED = True
 SECRET_KEY = 'goodgoodstudydaydayup'
 OPENID_PROVIDERS = [
-    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
-    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
-    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
-    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
-    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'}
+]
--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-#!/root/kengrary/microblog/flask/bin/python
 from app import app
 
-app.run(debug=True, host='0.0.0.0',port=5001)
+app.run(debug=True, host='0.0.0.0', port=5000)
